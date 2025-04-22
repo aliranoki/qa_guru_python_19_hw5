50bb38d7eae0dc97ff3e435afef3ad340956344b
Marina Filippova <alira.noki@gmail.com>
2025-04-22 22:11:05 +0300

    homework 9.2 добавление датакласса

--- demoqa_tests/model/User.py
+++ demoqa_tests/model/User.py
@@ -1,20 +1,20 @@
+from dataclasses import dataclass
 import datetime
 from enum import Enum
 
 
-# Перечисление для пола
 class Gender(Enum):
     MALE = "Male"
     FEMALE = "Female"
 
 
-# Перечисление для хобби
 class Hobbies(Enum):
     SPORT = "Sports"
     MUSIC = "Music"
     READING = "Reading"
 
 
+@dataclass
 class User:
     first_name: str
     last_name: str
@@ -28,18 +28,3 @@ class User:
     address: str
     state: str
     city: str
-
-    def __init__(self, first_name, last_name, user_email, gender, phone_number, date_of_birth, subjects, hobbies,
-                 picture, address, state, city):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.user_email = user_email
-        self.gender = gender
-        self.phone_number = phone_number
-        self.date_of_birth = date_of_birth
-        self.subjects = subjects
-        self.hobbies = hobbies
-        self.picture = picture
-        self.address = address
-        self.state = state
-        self.city = city
