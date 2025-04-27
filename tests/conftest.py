170ba7aa3ff595c202036c147fe2c61cdc7eb782
Marina Filippova <alira.noki@gmail.com>
2025-04-27 19:56:05 +0300

    updated setup_browser used headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,17 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 
 
 @pytest.fixture(scope="session")
 def setup_browser():
-    # Настройка опций Chrome
-    chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")  # Обход ограничений для Linux
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Для ограниченных ресурсов
-    chrome_options.add_argument("--headless")  # Режим без графического интерфейса
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
 
-    # Указываем путь к ChromeDriver (предполагается, что он установлен в /usr/local/bin/)
-    service = Service(executable_path='/usr/local/bin/chromedriver')
-
-    # Инициализация драйвера
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=options)
 
     # Настройка Selene
     browser.config.driver = driver
