b75205451d9e83b92cbb3eb80548c0d26a8953e7
Marina Filippova <alira.noki@gmail.com>
2025-04-27 19:27:17 +0300

    updated setup_browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
 import pytest
 from selene import browser
-from selene.support.shared import config
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+
 
 @pytest.fixture(scope="session")
 def setup_browser():
+    # Настройка ChromeDriver через WebDriver Manager
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
 
     yield
